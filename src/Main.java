bd2f7c479a0c88f3766ac1e3e6b4e7d9f8f38386
matheus <matheus.pereira@zup.com.br>
2024-12-12 17:55:18 -0300

    finalização da entrega máxima

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,37 @@
-import java.util.Scanner;
-import java.util.Random;
+import java.util.*;
 
 public class Main {
+
+        static List<Integer> acertos = new ArrayList<>();
+        static List<Integer> erros = new ArrayList<>();
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
-        int numeroSorteado = random.nextInt(10);
-        System.out.println("bem vindo ao jogo de adivinhação! para continuar escolha um palpite.");
-        int numeroEscolhido = scanner.nextInt();
+        int continua;
+        int pontuacao = 0;
 
-        while (!verificaNumero(numeroEscolhido))
-            numeroEscolhido = scanner.nextInt();
+        while (true) {
+            int numeroSorteado = 5 ;//random.nextInt(10);
+            System.out.println("bem vindo ao jogo de adivinhação! para continuar escolha um palpite.");
+            int numeroEscolhido = scanner.nextInt();
 
-        int pontuacao = pontuador(numeroEscolhido, numeroSorteado);
+            while (!validaNumero(numeroEscolhido))
+                numeroEscolhido = scanner.nextInt();
+            pontuacao += pontuador(numeroEscolhido,numeroSorteado);
+            System.out.println("sua pontuação total é: "+pontuacao);
 
+            System.out.println("uma nova rodada irá iniciar, para sair aperte 0 e para continuar aperte outro número");
+            continua = scanner.nextInt();
+            if (continua == 0) {
+                System.out.println("números certos: "+ acertos);
+                System.out.println("números errados: "+ erros);
+                return;
+            }
+        }
     }
-    static boolean verificaNumero(int esolhido){
-        if (esolhido < 0 | esolhido > 10){
+    static boolean validaNumero(int escolhido){
+        if (escolhido < 0 || escolhido > 10){
             System.out.println("número inválido, escolha um número entre 0 a 10");
             return false;
         }
@@ -28,20 +42,20 @@ public class Main {
 
         if(escolhido == sorteado)
         {
-            System.out.println("parabéns você acertou o número");
+            System.out.println("parabéns você acertou o número e ganhou 10 pontos");
             pontos += 10;
+            acertos.add(escolhido);
         }
-        else if(escolhido + 1 == sorteado | escolhido - 1 == sorteado) {
-            System.out.println("você quase acertou ");
+        else if(escolhido + 1 == sorteado || escolhido - 1 == sorteado) {
+            System.out.println("você quase acertou, ganhou 5 pontos");
             System.out.println("o número sorteado era: "+sorteado);
             pontos += 5;
         }
         else {
             System.out.println("infelizmente você não acertou");
             System.out.println("o número sorteado era: "+sorteado);
+            erros.add(escolhido);
         }
-        System.out.println("sua pontuação total é: "+pontos);
-
         return pontos;
     }
 }
